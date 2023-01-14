818f4c810887f60e14c1dbca25d59f312d317adf
pxg719 <62950904+pxg719@users.noreply.github.com>
2023-01-14 14:48:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -32,7 +32,7 @@ sed -i "s/OpenWrt /OpenWrt_x64_全功能版 by GXNAS build $(TZ=UTC-8 date "+%Y.
 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                             # 设置密码为空
 
-sed -i 's/PATCHVER:=5.4/PATCHVER:=5.15/g' target/linux/x86/Makefile                               # x86机型,默认内核5.4，修改内核为5.10
+#sed -i 's/PATCHVER:=5.4/PATCHVER:=5.15/g' target/linux/x86/Makefile                               # x86机型,默认内核5.4，修改内核为5.10
 
 # K3专用，编译K3的时候只会出K3固件
 #sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
